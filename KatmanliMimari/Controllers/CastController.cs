d4a5a86a3a454971b776e162981a91e5213e1f6e
abdussamet.solak <abdussamet66@gmail.com>
2022-09-08 11:37:51 +0300

    .

---
+++ KatmanliMimari/Controllers/CastController.cs
@@ -0,0 +1,55 @@
+﻿using Business.Concrete;
+using DataAccesLayer.Concrete.EntityFramework;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KatmanliMimari.Controllers
+{
+    public class CastController : Controller
+    {
+        // GET: Cast
+        CastManager castManager = new CastManager(new EfCastKadroDal());
+
+        public ActionResult Index()
+        {
+            var castList = castManager.list();
+            return View(castList);
+        }
+
+        [HttpGet]
+        public ActionResult AddCast()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCast(CastKadro castKadro)
+        {
+            castManager.addBL(castKadro);
+            return RedirectToAction("Index");
+        }
+        public ActionResult deleteCast(int id)
+        {
+            var cast = castManager.getById(id);
+            cast.durum = false;
+            castManager.update(cast);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult updateCast(int id)
+        {
+            var cast = castManager.getById(id);
+            return View(cast);
+        }
+        [HttpPost]
+        public ActionResult updateCast(CastKadro castKadro)
+        {
+            castManager.update(castKadro);
+            return RedirectToAction("Index");
+        }
+    }
+}
