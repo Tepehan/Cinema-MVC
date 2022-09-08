662989b4b6420ebe7a5c406da381c79ddd51b6c6
gozde.akman <gozdeakman617@hotmail.com>
2022-09-08 11:42:44 +0300

    .

--- Business/Concrete/SalonManager.cs
+++ Business/Concrete/SalonManager.cs
@@ -9,18 +9,35 @@ using System.Threading.Tasks;
 
 namespace Business.Concrete
 {
-   public class SalonManager : ISalonService
+    public class SalonManager : ISalonService
     {
         ISalonDal _salonDal;
-
         public SalonManager(ISalonDal salonDal)
         {
-            _salonDal = salonDal;
+            _salonDal = salonDal;        }
+
+        public void addBL(Salon salon)
+        {
+            _salonDal.Insert(salon);
+        }
+
+        public Salon getById(int id)
+        {
+            return _salonDal.GetById(x => x.salonId == id);
         }
 
         public List<Salon> list()
         {
-           return _salonDal.List();
+            return _salonDal.List();
+        }
+
+        public void update(Salon salon)
+        {
+            _salonDal.Update(salon);
+        }
+        public void DeleteBL(Salon salon)
+        {
+            _salonDal.Delete(salon);
         }
     }
 }
