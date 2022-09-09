da1a8addb1527f80b39943607b0a942f357af30e
abdussamet.solak <abdussamet66@gmail.com>
2022-09-09 10:46:08 +0300

    .

---
+++ Business/Validators/TurValidator.cs
@@ -0,0 +1,18 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Validators
+{
+    public class TurValidator: AbstractValidator<Tur>
+    {
+        public TurValidator()
+        {
+            RuleFor(t=>t.turIsim).MaximumLength(20).WithMessage("Maximum 20 karakter olabılır");           
+        }
+    }
+}
