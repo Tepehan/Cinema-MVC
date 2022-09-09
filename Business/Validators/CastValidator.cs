eba85e968e1127c7b353a56ca4c20956b0592fcb
abdussamet.solak <abdussamet66@gmail.com>
2022-09-09 12:14:14 +0300

    .

---
+++ Business/Validators/CastValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Validators
+{
+    public class CastValidator: AbstractValidator<CastKadro>
+    {
+        public CastValidator()
+        {
+            RuleFor(c => c.unvan).MaximumLength(20).WithMessage("Maximum 20 karakter olabılır");
+            RuleFor(c => c.ad).MaximumLength(20).WithMessage("Maximum 20 karakter olabılır");
+            RuleFor(c => c.soyad).MaximumLength(20).WithMessage("Maximum 20 karakter olabılır");
+            RuleFor(c => c.aciklama).MaximumLength(61).WithMessage("Maximum 61 karakter olabılır");
+        }
+    }
+}
