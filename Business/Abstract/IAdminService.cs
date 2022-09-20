b96bbc477579a1a71ad6f394ce9ea15ad13aa3f8
mehmet.ors <mhmt5658@gmail.com>
2022-09-20 14:59:47 +0300

    yeni oluşturuldu.

---
+++ Business/Abstract/IAdminService.cs
@@ -0,0 +1,21 @@
+﻿using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+   public interface IAdminService
+    {
+        void AddAdminBl(Admin admin);
+        void UpdateAdminBl(Admin admin);
+        void DeleteAdminBl(Admin admin);
+        List<Admin> Get();
+        Admin GetById(int id);
+
+        Admin Get(string userName, string password);
+
+    }
+}
