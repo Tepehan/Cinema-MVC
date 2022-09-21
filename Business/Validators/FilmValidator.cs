ea99b8cb99e7f1ac92340fdec8d43f0f23ca462c
gozde.akman <gozde.akman@SMPR303-09>
2022-09-21 10:54:10 +0300

    Slider admin tarafı yapıldı.

--- Business/Validators/FilmValidator.cs
+++ Business/Validators/FilmValidator.cs
@@ -15,6 +15,7 @@ namespace Business.Validators
             RuleFor(f => f.filmIsim).MaximumLength(20).WithMessage("Maximum 20 karakter olabılır");
             RuleFor(f => f.filmIsim).NotEmpty().WithMessage("Boş gecilmez");
             RuleFor(f => f.ozet).MaximumLength(400).WithMessage("Maximum 400 karakter olabılır");
+            RuleFor(f => f.ozet).MinimumLength(15).WithMessage("Minimum 15 karakter olabılır");
             RuleFor(f => f.ozet).NotEmpty().WithMessage("Boş gecilmez");
             RuleFor(f => f.dil).MaximumLength(13).WithMessage("Maximum 13 karakter olabılır");
             RuleFor(f => f.dil).NotEmpty().WithMessage("Boş gecilmez");
@@ -23,8 +24,7 @@ namespace Business.Validators
             RuleFor(f => f.destekledigiTeknolojiler).NotEmpty().WithMessage("Boş geçilemez");
             RuleFor(f => f.destekledigiTeknolojiler).MaximumLength(50).WithMessage("Maximum 50 karakter olabılır");
             RuleFor(f => f.butce).NotEmpty().WithMessage("Boş geçilemez");
-            RuleFor(f => f.afisUrl).MaximumLength(250).WithMessage("Maximum 250 karakter olabılır");
-            RuleFor(f => f.afisUrl).NotEmpty().WithMessage("Boş geçilemez");
+           
            
            
             RuleFor(f => f.durum).NotEmpty().WithMessage("Boş geçilemez");
