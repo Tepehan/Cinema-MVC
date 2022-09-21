c5e8cfe26fe953dce58f039116a7ca4aa189677c
hacer.ustacolak <hacer__usta@hotmail.com>
2022-09-21 13:08:11 +0300

    Dinamik menü için çalışmalar yapıldı.

---
+++ Business/Concrete/MenuManager.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstract;
+using DataAccesLayer.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class MenuManager : IMenuService
+    {
+        IMenuDal _menuDal;
+
+        public MenuManager(IMenuDal menuDal)
+        {
+            _menuDal = menuDal;
+        }
+
+        public void AddBL(Menu menu)
+        {
+            _menuDal.Insert(menu);
+        }
+        public void DeleteBL(Menu menu)
+        {
+            _menuDal.Delete(menu);
+        }
+        public void UpdateBL(Menu menu)
+        {
+            _menuDal.Update(menu);
+        }
+        public Menu GetByIdBL(int id)
+        {
+            return _menuDal.GetById(x => x.menuId == id);
+        }
+        public List<Menu> ListBL()
+        {
+            return _menuDal.List();
+        }
+
+    }
+}
---
+++ Entity/Concrete/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class Menu
+    {
+        [Key]
+        public int menuId { get; set; }
+        public string menuName { get; set; }
+        public int parentId { get; set; }
+        public virtual Menu parent { get; set; }
+        public ICollection<Menu> children { get; set; }
+    }
+}
