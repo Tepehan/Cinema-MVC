532449399732608cb949cb77e209ba1279aca160
enes.tepehan <enestepehan44@gmail.com>
2022-09-26 14:32:50 +0300

    Dynamic sub menu added

--- Entity/Concrete/Menu.cs
+++ Entity/Concrete/Menu.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,11 @@ namespace Entity.Concrete
         [Key]
         public int menuId { get; set; }
         public string menuName { get; set; }
-        public int parentId { get; set; }
-        public virtual Menu parent { get; set; }
-        public ICollection<Menu> children { get; set; }
+        [ForeignKey("parent")]
+        
+        public int? parentId { get; set; }
+        public virtual Menu parent { get; set;}
+        [InverseProperty("parent")]
+        public ICollection<Menu> children { get; set;}
     }
 }
