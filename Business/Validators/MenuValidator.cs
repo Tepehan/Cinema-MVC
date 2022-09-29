432358274a157aa32a89c2e2f373fe93f683f7a0
hacer.ustacolak <hacer__usta@hotmail.com>
2022-09-29 10:02:19 +0300

    Menu için validasyon yapıldı.

---
+++ Business/Validators/MenuValidator.cs
@@ -0,0 +1,24 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Validators
+{
+    public class MenuValidator : AbstractValidator<Menu>
+    {
+        public MenuValidator()
+        {
+           
+            RuleFor(f => f.menuName).MaximumLength(50).WithMessage("Maximum 50 karakter olabılır");
+            RuleFor(f => f.menuName).NotEmpty().WithMessage("Boş gecilmez");
+            RuleFor(f => f.durum).NotEmpty().WithMessage("Boş gecilmez");
+
+
+
+        }
+    }
+}
