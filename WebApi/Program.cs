3ed47d9d842b08eb46615b2ee9ba11d3f522a18a
enes.tepehan <enestepehan44@gmail.com>
2022-10-06 12:00:07 +0300

    .

---
+++ WebApi/Program.cs
@@ -0,0 +1,37 @@
+using DataAccesLayer;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling =
+       Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
