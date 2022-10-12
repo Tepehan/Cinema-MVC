3e02c27696de49f9ba12fe2193e98b53774c0d53
gozde.akman <gozde.akman@SMPR303-09>
2022-10-12 14:20:04 +0300

    üye ol işlmeleri yapıldı

---
+++ Business/Abstract/IMusteriService.cs
@@ -0,0 +1,18 @@
+﻿using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IMusteriService
+    {
+        void AddMusteri(Musteri musteri);
+        void UpdateMusteri(Musteri musteri);
+        void DeleteMusteri(Musteri musteri);
+        List<Musteri> Get();
+
+    }
+}
