548ede9cc8ef71957cb6496626f4f32039266b6c
gozde.akman <gozde.akman@SMPR303-09>
2022-10-12 14:20:52 +0300

    üye ol işlmeleri yapıldı

---
+++ KatmanliMimari/Controllers/UyeOlController.cs
@@ -0,0 +1,38 @@
+﻿using Business.Concrete;
+using DataAccesLayer.Concrete.EntityFramework;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace KatmanliMimari.Controllers
+{
+    public class UyeOlController : Controller
+    {
+        MusteriManager musteriManager = new MusteriManager(new EfMusteriDal());
+        // GET: UyeOl
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Musteri musteri)
+        {
+            if (ModelState.IsValid)
+            {
+                musteriManager.AddMusteri(musteri);
+            }
+
+            return View();
+            
+            
+
+        }
+
+    }
+}
