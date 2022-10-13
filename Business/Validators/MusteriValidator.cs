1a9a1d650dbf0fbeb5cdf6df349adce3d9140bfb
gozde.akman <gozde.akman@SMPR303-09>
2022-10-13 14:33:31 +0300

    üye işlemleri yapıldı

---
+++ Business/Validators/MusteriValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Validators
+{
+    public class MusteriValidator : AbstractValidator<Musteri>
+    {
+        public MusteriValidator()
+        {
+            RuleFor(f => f.ad).NotEmpty().WithMessage("Boş gecilmez");
+            RuleFor(f => f.soyad).NotEmpty().WithMessage("Boş gecilmez");
+            RuleFor(f => f.eMail).NotEmpty().WithMessage("Boş gecilmez");
+            RuleFor(f => f.sifre).NotEmpty().WithMessage("Boş geçilemez");
+            RuleFor(f => f.telefonNumara).NotEmpty().WithMessage("Boş geçilemez");
+        }
+
+    }
+}
