96014e9b16edbe939801fb16b57b03e5157a782a
gozde.akman <gozde.akman@SMPR303-09>
2022-10-13 14:33:50 +0300

    üye işlemleri yapıldı

--- DataAccesLayer/Concrete/EntityFramework/EfMusteriDal.cs
+++ DataAccesLayer/Concrete/EntityFramework/EfMusteriDal.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DataAccesLayer.Abstract;
+using DataAccesLayer.Concrete.Repositories;
+using Entity.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccesLayer.Concrete.EntityFramework
 {
-    public class EfMusteriDal 
+    public class EfMusteriDal  :GenericRepository<Musteri>, IMusteriDal
     {
     }
 }
