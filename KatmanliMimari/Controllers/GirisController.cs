62e5a077a4f83253d3b97b6a6aca71781dec50b3
GozdeAkmann <gozdeakman617@hotmail.com>
2022-10-25 11:55:08 +0300

    giriş front end tarafı yazıldı.

---
+++ KatmanliMimari/Controllers/GirisController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KatmanliMimari.Controllers
+{
+    public class GirisController : Controller
+    {
+        // GET: Giris
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
